4bf2ea8ddb74bf189cd5b16d95d3f3d2fb87f12e
sjisntsuperman <weishijiestein@outlook.com>
2020-11-07 06:20:48 +0800

    feat: first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Router = require('koa-router')
+const KOA = require('koa')
+
+const app = new KOA()
+const router = new Router()
+app.use(router.routes()).use(router.allowedMethods())
+
+router.get('/', function (ctx) {
+    ctx.body='hello serverless from koa'
+})
+
+// app.listen(3000)
+
+// module.exports = app
